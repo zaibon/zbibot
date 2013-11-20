66f2756e5f0256eeeb0763d0ced3e468705f495c
Christophe de Carvalho <christophe.dcpm@gmail.com>
2013-11-20 23:27:39 +0100

    flag
    server
    port
    ssl
    channel

--- main.go
+++ main.go
@@ -1,19 +1,55 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"github.com/Zaibon/ircbot"
-	// "time"
 )
 
+type channels []string
+
+func (c *channels) String() string {
+	return fmt.Sprintf("%s", *c)
+}
+
+func (c *channels) Set(value string) error {
+	*c = append(*c, value)
+	return nil
+}
+
+var (
+	flagServer   string
+	flagPort     string
+	flagSsl      bool
+	flagChannels channels
+)
+
+func init() {
+	flag.StringVar(&flagServer, "server", "irc.freenode.net", "ip adresse of the server you want to connect to")
+	flag.StringVar(&flagPort, "port", "6697", "port")
+	flag.BoolVar(&flagSsl, "ssl", true, "true|false")
+
+}
+
 func main() {
+	flag.Var(&flagChannels, "c", "channels")
+
+	flag.Parse()
 
 	b := ircbot.NewIrcBot()
-	b.Server = "irc.freenode.net"
-	b.Port = "6667"
-	b.Nick = "ZbiBot"
+	b.Server = flagServer
+	b.Port = flagPort
+	b.Encrypted = flagSsl
+	b.Nick = "ZbiBotTLS"
 	b.User = b.Nick
 
-	b.Channel = append(b.Channel, "#testgigx")
+	if flag.NFlag() != 0 {
+		for i := 0; i < len(flagChannels); i++ {
+			b.Channel = append(b.Channel, flagChannels[i])
+		}
+	}
+
+	fmt.Println(b)
 
 	b.Handlers["PING"] = ircbot.Pong
 	b.Handlers["JOIN"] = ircbot.Join
